75edb008aec27bed0dac69994fe79bceebe401e0
azamat <azamatbekxoshimov571>
2025-02-11 16:10:03 +0500

    e

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,6 @@ from configapp2.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('configapp.urls')),
-    path('app1/', include('configapp.urls')),
-    path('app2/', include('configapp.urls')),
-    path('app3/', include('configapp1.urls')),
-    path('app4/', include('configapp1.urls')),
-    path('app5/', include('configapp1.urls')),
-    path('app6/', include('configapp2.urls')),
-    path('app7/', include('configapp2.urls')),
-    path('app8/', include('configapp2.urls')),
+    path('app1/', include('configapp1.urls')),
+    path('app2/', include('configapp2.urls')),
 ]
